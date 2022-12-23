7d92f34e75527bb7a5c72435a495fc6fe21a583f
Monty Hindman <mhindman@apple.com>
2022-12-23 12:13:21 -0800

    setup.py: added classifiers

--- setup.py
+++ setup.py
@@ -51,20 +51,14 @@ readme_file = join(project_dir, 'README.md')
 long_desc = open(readme_file).read()
 long_desc_type = 'text/markdown'
 
-# classifiers = [
-#     'Development Status :: 3 - Alpha',
-#     'Environment :: Console',
-#     'Intended Audience :: Developers',
-#     'License :: OSI Approved :: MIT License',
-#     'Operating System :: OS Independent',
-#     'Programming Language :: Python :: 3.5',
-#     'Programming Language :: Python :: 3.6',
-#     'Programming Language :: Python :: 3.7',
-#     'Programming Language :: Python :: 3.8',
-#     'Programming Language :: Python :: 3.9',
-#     'Programming Language :: Python :: 3.10',
-#     'Topic :: Software Development',
-# ]
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development',
+]
 
 ####
 # Packages and scripts.
@@ -97,6 +91,6 @@ setup(
     license = license,
     long_description = long_desc,
     long_description_content_type = long_desc_type,
-    # classifiers = classifiers,
+    classifiers = classifiers,
 )
 
